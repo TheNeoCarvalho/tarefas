1b9aa2644b318d1e53fa4e2c86b553534f849995
Manoel Carvalho <manoelmecat@gmail.com>
2024-11-07 16:39:36 -0300

    feat: auth and page dash

--- src/pages/dash/index.tsx
+++ src/pages/dash/index.tsx
@@ -1,5 +1,9 @@
+import { GetServerSideProps } from 'next'
 import styles from './dash.module.css'
 import Head from 'next/head'
+
+import { getSession } from 'next-auth/react'
+import { TextArea } from '../../components/TextArea'
 
 export default function Dash(){
     return (
@@ -7,8 +11,43 @@ export default function Dash(){
         <Head>
             <title>Meu painel - Dash</title>
         </Head>
-        <h1>Dash</h1>
+       <main className={styles.main}>
+        <section className={styles.content}>
+            <div className={styles.contentForm}>
+                <h1 className={styles.title}>
+                    Qual sua tarefa?
+                </h1>
+                <form>
+                    <TextArea />
+                    <div className={styles.checkBoxArea}>
+                        <input 
+                            type="checkbox"
+                            className={styles.checkbox}
+                        />
+                        <label>Deixar tarefa publica?</label>
+                    </div>
+                    <button type="submit" className={styles.button}>Registar</button>
+                </form>
+            </div>
+        </section>
+       </main>
         </div>
         
     )
+}
+
+export const getServerSideProps: GetServerSideProps = async ( {req} ) => {
+    const session = await getSession({ req })
+
+    if(!session?.user) {
+        return  {
+            redirect: {
+                destination: '/',
+                permanent: false
+            }
+        }
+    }
+    return {
+        props: {}
+    }
 }
